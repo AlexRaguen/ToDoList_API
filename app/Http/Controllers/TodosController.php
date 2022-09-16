89925df0ac990d6a2408cd92934e3fd2b5a362b6
AlexandreRaguenes <a.raguenes@gmail.com>
2022-09-16 11:57:23 +0200

    installation Laravel et mise en place DB

---
+++ app/Http/Controllers/TodosController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Todos;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class TodosController extends Controller
+{
+    //
+}
